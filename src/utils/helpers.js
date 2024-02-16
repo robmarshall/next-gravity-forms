905522ca0e5d269eb54bf59344a14aa817121598
Bohdan <bohdan@gohike.nl>
2024-02-16 14:54:15 +0200

    Removed duplicated function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31,9 +31,3 @@ export const interpolateString = (template, values) => {
 export const isNonEmptyObject = (obj) => {
   return obj !== null && typeof obj === "object" && Object.keys(obj).length > 0;
 };
-
-export const interpolateString = (template, values) => {
-  return template.replace(/\{\{(\w+)\}\}/g, (placeholder, key) => {
-    return values[key] || placeholder;
-  });
-};
