1b95bee0d73f930f15b1ac625a1813e5a96e2774
slavic_hike <slavic@gohike.nl>
2024-03-01 17:10:15 +0200

    add single file upload test

--- tests/components/fields/FileUpload.test.js
+++ tests/components/fields/FileUpload.test.js
@@ -2,14 +2,24 @@ import renderGravityForm from "../render";
 import { screen } from "@testing-library/dom";
 import mockFormData from "../../mocks/formData";
 import { getByText, fireEvent, waitFor, act } from "@testing-library/react";
-import { submitGravityForm } from "../../../src/fetch";
+import {
+  submitGravityForm,
+  submitFormWithFile,
+  fetchAPI,
+} from "../../../src/fetch";
 
 // mock submit so we don't run real request
 jest.mock("../../../src/fetch", () => ({
   submitGravityForm: jest.fn(),
+  submitFormWithFile: jest.fn(),
+  fetchAPI: jest.fn(),
+  fetch: jest.fn(),
 }));
 
 describe("File upload field", () => {
+  let container;
+  let element;
+
   const field = {
     id: 11,
     cssClass: null,
@@ -19,13 +29,28 @@ describe("File upload field", () => {
     isRequired: true,
     label: "File",
     value: null,
-    allowedExtensions: ["pdf", "exe"],
+    allowedExtensions: ["pdf", "exe"], //allow exe to test whether server rejects it
     canAcceptMultipleFiles: false,
     inputType: null,
     maxFileSize: 1, // MB
     maxFiles: null,
     type: "FILEUPLOAD",
   };
+
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ data: "mockData" }),
+      })
+    );
+
+    const rendered = renderGravityForm({
+      gfForm: { formFields: { nodes: [field] } },
+    });
+    container = rendered.container;
+
+    element = container.querySelector(`#${fieldId}`);
+  });
 
   afterEach(() => {
     jest.resetAllMocks();
@@ -34,83 +59,72 @@ describe("File upload field", () => {
   const fieldId = `field_${mockFormData.gfForm.databaseId}_${field.id}`;
 
   it("renders correctly", async () => {
-    const { container } = renderGravityForm({
-      gfForm: { formFields: { nodes: [field] } },
-    });
-    const element = container.querySelector(`#${fieldId}`);
-
-    // email field rendered
+    // upload field rendered
     expect(element).toBeInTheDocument();
+
+    // displays rules
+    expect(
+      container.querySelector(`.gform_fileupload_rules`)
+    ).toHaveTextContent("Accepted file types: pdf, exe. Max. file size: 1MB");
   });
 
   describe("handle errors", () => {
-    let container;
-    let element;
-    beforeEach(() => {
-      const rendered = renderGravityForm({
-        gfForm: { formFields: { nodes: [field] } },
-      });
-      container = rendered.container;
+    it("should display error when file type is not correct", async () => {
+      const fileInput = screen.getByLabelText(/File/i);
 
-      element = container.querySelector(`#${fieldId}`);
+      // mock file
+      const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+      // add value
+      fireEvent.change(fileInput, { target: { files: [file] } });
+
+      fireEvent.submit(screen.getByRole("button"));
+
+      await waitFor(() => {
+        expect(
+          getByText(
+            element,
+            /The uploaded file type is not allowed. Must be one of the following/i
+          )
+        ).toBeInTheDocument();
+      });
     });
 
-    // it("should display error when file type is not correct", async () => {
-    //   const fileInput = screen.getByLabelText(/File/i);
+    it("rejects a file larger than 1MB", async () => {
+      const fileInput = screen.getByLabelText(/File/i);
 
-    //   // mock file
-    //   const file = new File(["hello"], "hello.png", { type: "image/png" });
+      // mock file
+      const file = new File(
+        [new ArrayBuffer(1024 * 1024 * 2)],
+        "largeFile.png",
+        {
+          type: "image/png",
+        }
+      );
 
-    //   // add value
-    //   fireEvent.change(fileInput, { target: { files: [file] } });
+      // add value
+      fireEvent.change(fileInput, { target: { files: [file] } });
 
-    //   fireEvent.submit(screen.getByRole("button"));
+      fireEvent.submit(screen.getByRole("button"));
 
-    //   await waitFor(() => {
-    //     expect(
-    //       getByText(
-    //         element,
-    //         /The uploaded file type is not allowed. Must be one of the following/i
-    //       )
-    //     ).toBeInTheDocument();
-    //   });
-    // });
+      await waitFor(() => {
+        expect(
+          getByText(element, /File exceeds size limit. Maximum file size/i)
+        ).toBeInTheDocument();
+      });
+    });
 
-    // it("file input rejects a file larger than 1MB", async () => {
-    //   const fileInput = screen.getByLabelText(/File/i);
+    it("displays error when file is required and no value added", async () => {
+      fireEvent.submit(screen.getByRole("button"));
 
-    //   // mock file
-    //   const file = new File(
-    //     [new ArrayBuffer(1024 * 1024 * 2)],
-    //     "largeFile.png",
-    //     {
-    //       type: "image/png",
-    //     }
-    //   );
+      await waitFor(() => {
+        expect(
+          getByText(element, /This field is required/i)
+        ).toBeInTheDocument();
+      });
+    });
 
-    //   // add value
-    //   fireEvent.change(fileInput, { target: { files: [file] } });
-
-    //   fireEvent.submit(screen.getByRole("button"));
-
-    //   await waitFor(() => {
-    //     expect(
-    //       getByText(element, /File exceeds size limit. Maximum file size/i)
-    //     ).toBeInTheDocument();
-    //   });
-    // });
-
-    // it("display error when file is required and no value added", async () => {
-    //   fireEvent.submit(screen.getByRole("button"));
-
-    //   await waitFor(() => {
-    //     expect(
-    //       getByText(element, /This field is required/i)
-    //     ).toBeInTheDocument();
-    //   });
-    // });
-
-    it("display error when user tries to upload risky file", async () => {
+    it("displays error when user tries to upload risky file", async () => {
       // Mock the submitGravityForm function to reject with the specific error
       submitGravityForm.mockRejectedValueOnce({
         data: {
@@ -128,10 +142,6 @@ describe("File upload field", () => {
       const fileInput = screen.getByLabelText(/File/i);
 
       // mock risky file
-      // const file = new File(["hello"], "hello.png", {
-      //   type: "image/png",
-      // });
-
       const file = new File(["hello"], "hello.exe", {
         type: "application/x-msdownload",
       });
@@ -145,8 +155,45 @@ describe("File upload field", () => {
       });
 
       expect(submitGravityForm).toBeCalled();
+    });
+  });
 
-      screen.debug();
+  describe("upload works", () => {
+    // Mock fetch globally
+    global.fetch = jest.fn();
+
+    beforeEach(() => {
+      fetch.mockClear();
+    });
+
+    it("able to upload", async () => {
+      submitGravityForm.mockResolvedValueOnce({
+        data: {
+          submitGfForm: {
+            errors: [],
+          },
+        },
+      });
+
+      const fileInput = screen.getByLabelText(/File/i);
+
+      // mock file
+      const file = new File([new ArrayBuffer(1024)], "upload.pdf", {
+        type: "application/pdf",
+      });
+
+      await act(async () => {
+        fireEvent.change(fileInput, { target: { files: [file] } });
+      });
+
+      await act(async () => {
+        fireEvent.submit(screen.getByRole("button", { name: "Submit1" }));
+      });
+
+      expect(submitGravityForm).toBeCalled();
+      expect(
+        submitGravityForm.mock.calls[0][0].fieldValues[0].fileUploadValues
+      ).toEqual([file]);
     });
   });
 });
