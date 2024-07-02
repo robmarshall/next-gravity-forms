edf1e4c87e9702902824551d1795cfdefa5838ea
slavic_hike <slavic@gohike.nl>
2024-07-02 16:09:31 +0300

    import date-picker dynamically

--- src/components/Date/Picker/index.js
+++ src/components/Date/Picker/index.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import DatePicker from "react-datepicker";
 import { Controller } from "react-hook-form";
 import { useSettings } from "../../../providers/SettingsContext";
 import { valueToLowerCase, interpolateString } from "../../../utils/helpers";
 import CalendarIconComponent from "./CalendarIconComponent";
 import { isValidDate } from "./helpers";
+
+const DatePicker = lazy(() => import("react-datepicker"));
+
 // import "react-datepicker/dist/react-datepicker.css";
 
 const dateFormats = {
@@ -64,7 +66,7 @@ const Picker = ({ fieldData, name, control, presetValue }) => {
       control={control}
       defaultValue={getProperDefault()}
       render={({ field: { onChange, value } }) => (
-        <>
+        <Suspense>
           <DatePicker
             selected={value}
             id={name}
@@ -99,7 +101,7 @@ const Picker = ({ fieldData, name, control, presetValue }) => {
               {datepicker.screenReaderText[dateFormat]}
             </span>
           )}
-        </>
+        </Suspense>
       )}
       rules={{
         required: isRequired && (errorMessage || strings.errors.required),
