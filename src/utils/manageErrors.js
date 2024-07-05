2c8120ea78e8a30698e498cd8e40f1e23940fe64
slavic_hike <slavic@gohike.nl>
2024-07-05 13:07:52 +0300

    Revert "Revert "fix submission after changing input id""
    This reverts commit c4715a7d00e77fd0d80eb7d9468a23b483580a4f.

--- src/utils/manageErrors.js
+++ src/utils/manageErrors.js
@@ -2,18 +2,20 @@
  * Maps errors from a response into a structured object and optionally sets them into a form state.
  *
  * @param {Array} data - Array of error objects from the response.
+ * @param {Number} databaseId - form ID
  * @param {Function} setError - Function to set error messages in a form state.
  *                              This is optional and defaults to a no-operation function.
- * @returns {Object} An object mapping of errors in the format: { 'input_id': { type, message } }.
+ * @returns {Object} An object mapping of errors in the format: { 'input_formID_id': { type, message } }.
  */
 export const handleGravityFormsValidationErrors = (
   data,
+  databaseId,
   setError = () => {}
 ) => {
   const errorsObject = {};
 
   data?.forEach((error) => {
-    const errorKey = `input_${error.id}`;
+    const errorKey = `input_${databaseId}_${error.id}`;
     const errorValue = { type: "gf_validation", message: error.message };
     errorsObject[errorKey] = errorValue;
 
