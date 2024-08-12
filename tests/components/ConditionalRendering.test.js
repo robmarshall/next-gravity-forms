77af3df96832831ec44ba9a954f079fe81054aa3
slavic_hike <slavic@gohike.nl>
2024-08-12 16:26:10 +0300

    add tests for required fields while conditional rendering

--- tests/components/ConditionalRendering.test.js
+++ tests/components/ConditionalRendering.test.js
@@ -1,6 +1,12 @@
 import { screen, fireEvent, act } from "@testing-library/react";
 import renderGravityForm from "./render";
 import mockFormData from "../mocks/formData";
+import { submitGravityForm } from "../../src/fetch";
+
+// mock submit so we don't run real request
+jest.mock("../../src/fetch", () => ({
+  submitGravityForm: jest.fn(),
+}));
 
 describe("Conditional logic", () => {
   const fields = [
@@ -291,5 +297,59 @@ describe("Conditional logic", () => {
     const style = window.getComputedStyle(target);
 
     expect(style.display).toBe("none");
+  });
+
+  // check if form ignores conditionally hidden required fields
+  it("doesn't count a required field if it is conditionally hidden", async () => {
+    const fields = [
+      {
+        id: 1,
+        type: "TEXT",
+        label: "Single",
+        isRequired: true,
+        conditionalLogic: {
+          rules: [
+            {
+              fieldId: 2,
+              operator: "IS",
+              value: "second",
+            },
+          ],
+          logicType: "ANY",
+          actionType: "SHOW", // hidden by default, but required
+        },
+      },
+      {
+        id: 2,
+        type: "TEXT",
+        label: "Second",
+        defaultValue: "test",
+      },
+    ];
+
+    const { container } = renderGravityForm({
+      data: {
+        gfForm: {
+          formFields: {
+            nodes: fields,
+          },
+        },
+      },
+    });
+
+    await act(async () => {
+      fireEvent.submit(container.querySelector(`#gform_submit_button_2`));
+    });
+
+    // we check if there is file value being processed, we don't check actual uploading
+    expect(submitGravityForm).toBeCalledWith({
+      id: mockFormData.gfForm.databaseId,
+      fieldValues: [
+        {
+          value: "test",
+          id: 2,
+        },
+      ],
+    });
   });
 });
